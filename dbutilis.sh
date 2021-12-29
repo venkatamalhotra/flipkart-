63660177868cc534656083818f4ceb3e93ef46d9
malhotra.venkata <malhotra.venkata@gmail.com>
2021-12-29 15:34:35 +0400

    updated

--- dbutilis.sh
+++ dbutilis.sh
@@ -2,5 +2,6 @@ Hello guys
 ho war eyou 
 Thi sfile contains DB coonetcion logic
 
-this is the updated file 
+this is the updated file
+switch to the Branch name  from master to development Branch 
 `
