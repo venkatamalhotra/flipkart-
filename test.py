bc21be746d148408e5c092b1f2c136e567f0771f
malhotravenkata <93545197+malhotravenkata@users.noreply.github.com>
2021-12-30 12:01:50 +0400

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,9 @@
 how are you 
  >>>>>>>
  here update the file 
- iam sending pull request Form QA to MASTER CODE 
+ 
+ iam sending pull request from QA to MASTER CODE 
+ This file contains into QA 
 
  now i am in Prod Branch 
 
